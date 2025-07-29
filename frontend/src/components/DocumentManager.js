1c1b8ac1f644d4876af9615a7df62684541f3636
Gockki <jere.kokki@gmail.com>
2025-07-29 21:55:13 +0300

    Fix DocumentManager API_BASE to use environment variable

--- frontend/src/components/DocumentManager.js
+++ frontend/src/components/DocumentManager.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 const DocumentManager = ({ documents, userEmail, onDocumentDeleted, onRefresh }) => {
   const [deleteModal, setDeleteModal] = useState(null);
   const [isDeleting, setIsDeleting] = useState(false);
-  const API_BASE = 'http://localhost:8000';
+  const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
   const deleteDocument = async (doc) => {
     setIsDeleting(true);
